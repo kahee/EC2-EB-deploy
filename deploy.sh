e4d3bb4006f9d34816dcbf8e540f863ac947b76d
kahee <yuygh131@gmail.com>
2018-03-29 11:09:39 +0900

    deploy.sh 수정1

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 #2. eb-deploy시, .secrets를 실행하고 배포 한후, .secrets을 지워줌
-git add -f .secrets && eb deploy --staged --profile=EB; git reset HEAD .secrets
+git add -f .secrets && eb deploy --staged --profile = EB; git reset HEAD .secrets
